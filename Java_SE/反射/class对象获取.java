08ed5dfa76165d5dbdd18735bd90a0598f946808
核桃圆 <47206212+3209613125@users.noreply.github.com>
2019-04-16 20:10:44 +0800

    Create class对象获取.java

---
+++ Java_SE/反射/class对象获取.java
@@ -0,0 +1,22 @@
+package www.bit.tech;
+import www.bit.tech.Fanshe1;
+class Test3 {
+    public static void main(String[] args) {
+        Class reflect = Fanshe1.class;
+        Fanshe1 fanshe = new Fanshe1();
+        Class reflect1 = fanshe.getClass();
+        try {
+            Class reflect3 = Class.forName("www.bit.tech.Fanshe1");
+            System.out.println(reflect3);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("没有找到");
+        }
+        System.out.println(reflect);
+        System.out.println(reflect1);
+        // System.out.println(reflect3);
+        System.out.println(reflect == reflect1);
+        System.out.println(reflect.equals(reflect1));
+
+    }
+}
