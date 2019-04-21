b4cf4f809b356b58bc4a85a1f89c16320ce9fd8f
核桃圆 <47206212+3209613125@users.noreply.github.com>
2019-04-21 22:08:53 +0800

    Create 部分内容输出.java

---
+++ Java_SE/IO流/部分内容输出.java
@@ -0,0 +1,15 @@
+//部分内容输出
+
+//import java.io.*;
+//class Test{
+//    public static void main(String[] args)throws IOException {
+//        File file = new File(File.separator + "Users" + File.separator + "acer" + File.separator + "Desktop"+ File.separator + "hello.txt");
+//   if(!file.getParentFile().exists()){
+//       file.getParentFile().mkdirs();
+//   }
+//   OutputStream output=new FileOutputStream(file,true);
+//   String msg="泰勒斯威夫特";
+//   output.write(msg.getBytes(),0,2);
+//   output.close();
+//    }
+//}
