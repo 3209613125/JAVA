191a550652dc3f306606770a1d1f4c273fc18a5e
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-04-28 12:38:17 +0800

    Create Random方法.java

---
+++ Java常用API/Random方法.java
@@ -0,0 +1,10 @@
+public class yg {
+    public static void main(String[] args) {
+
+        for(int s=0;s<10;s++){
+            double a= (double) (Math.random()*3);
+            System.out.println(a);
+        }
+    }
+}
+//默认值为基本数据类型的默认值，左闭（默认值），右开（* x）
