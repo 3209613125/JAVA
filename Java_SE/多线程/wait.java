70bfda0d90703e435017c059037060a1ff3e180a
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-04-28 11:26:48 +0800

    Create wait.java

---
+++ Java_SE/多线程/wait.java
@@ -0,0 +1,16 @@
+class TestWait {
+
+    public static void main(String[] args) {
+        Object object = new Object();
+        synchronized(object) {
+            System.out.println("开始执行");
+            try {
+                object.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("结束执行");
+        }
+
+    }
+}
