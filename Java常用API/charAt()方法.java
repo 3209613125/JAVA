3f56b786afdd06dbf161a853c18bf3075a51d31b
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-05-31 20:11:37 +0800

    Create charAt()方法.java

---
+++ Java常用API/charAt()方法.java
@@ -0,0 +1,20 @@
+//charAt()方法用于返回指定索引出的字符，索引范围从0到length（）-1.
+
+//public char charAt(int index)
+
+//返回值：返回指定索引处的字符
+
+public class Test{
+
+public static void main(String args[]){
+
+String d="m.runoob.com";
+
+char result = s.charAt(8);
+
+System.out.println(result);
+}
+
+}
+
+//执行结果：'.';
