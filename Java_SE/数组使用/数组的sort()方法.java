5451dc17d7b822e5b7b5301cf79daa49a1c4f050
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-11 22:13:53 +0800

    Update 数组的sort()方法.java

--- Java_SE/数组使用/数组的sort()方法.java
+++ Java_SE/数组使用/数组的sort()方法.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 class Test{
     public static void main(String[] args) {
-        int arr[] = {9,8,7,6};
+        int[] arr = {9,8,7,6};
         Arrays.sort(arr);
         for(int i : arr){
             System.out.println(i);
@@ -10,5 +10,7 @@ class Test{
     }
 }
 
-
 //sort方法默认按照升序排列，即从小到大
+
+
+//java.util.Arrays.sort(a);    或者    import java.util.Arrays;Arrays.sort()
