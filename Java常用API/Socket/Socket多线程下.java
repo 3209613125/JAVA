8fc1ea8aa7f3300213de53994f06310dc6babf99
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-11-12 08:03:13 +0800

    Update Socket多线程下.java

--- Java常用API/Socket/Socket多线程下.java
+++ Java常用API/Socket/Socket多线程下.java
@@ -53,3 +53,61 @@ public class Solution{
         new send().start();
     }
 }
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+class send implements Runnable{
+    @Override
+    public void run() {
+       try {
+           Scanner scanner = new Scanner(System.in);
+           DatagramSocket socket = new DatagramSocket();
+           while (true){
+               String str = scanner.nextLine();
+               DatagramPacket packet = new DatagramPacket(str.getBytes(),str.getBytes().length,InetAddress.getByName("127.0.0.1"),6666);
+               if (str.equals("quit")){
+                   break;
+               }
+               else{
+                   socket.send(packet);
+               }
+           }
+           socket.close();
+       }catch (Exception e){
+           e.printStackTrace();
+       }
+    }
+
+}
+class receive implements Runnable{
+    @Override
+    public void run() {
+     try {
+         DatagramSocket socket = new DatagramSocket(6666);
+         DatagramPacket packet = new DatagramPacket(new byte[1024],1024);
+         while (true){
+             socket.receive(packet);
+             byte[] str = packet.getData();
+             int len = packet.getLength();
+             String ip = packet.getAddress().getHostAddress();
+             int port = packet.getPort();
+             System.out.println("ip "+ip+":"+"port "+port+" "+new String(str,0,len));
+         }
+     }catch (Exception e){
+         e.printStackTrace();
+     }
+    }
+}
+
+class Test{
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(new receive()).start();
+        Thread.sleep(10);
+        new Thread(new send()).start();
+    }
+}
+
+
